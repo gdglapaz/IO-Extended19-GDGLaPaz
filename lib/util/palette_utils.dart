f7fdb7a8f5c520345e21193478b352c650732cc7
luis_mantilla <lmmantilla9350@gmail.com>
2019-05-17 01:20:12 -0400

    Add Palette Colors

---
+++ lib/util/palette_utils.dart
@@ -0,0 +1,9 @@
+//Colors
+
+int primaryColor = 0xFF8500CC;
+int secondaryColor = 0xFF8500CC;
+int accentColor = 0xFF8500CC;
+int primaryText = 0xFF8A0000;
+int secondaryText = 0xFF8500CC;
+int dividerColor = 0xFF8500CC;
+
