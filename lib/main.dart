efee7a8ef0e0d21f657fdda7d79bafdb96ffcdfc
alexiamollo <alexiamollo123@gmail.com>
2019-05-17 23:39:57 -0400

    Pantalla SpeakerDetail completa

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:io_extended_gdglapaz/screens/home/home.dart';
+import 'package:io_extended_gdglapaz/screens/speakerDetail/speakerDetail.dart';
 
 void main(){
   runApp(
-    HomeScreen()
+    SpeakerDetail("https://www.websa100.com/wp-content/uploads/2016/05/foto-en-blanco-y-negro.png", "Full Name", "Google Expert in Angular")
   );
 }
