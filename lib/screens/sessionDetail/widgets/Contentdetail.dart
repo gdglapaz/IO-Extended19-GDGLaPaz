2d3935eaad87541921c338c1aba9ae5d5cb1dfcf
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-05-18 16:39:31 -0400

    Add UI sessionDetail

---
+++ lib/screens/sessionDetail/widgets/Contentdetail.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Contentdetail extends StatelessWidget{
+
+  String aboutSpeaker;
+  List<String> nameTechnologies;
+  Contentdetail(this.aboutSpeaker, this.nameTechnologies);
+
+  @override
+  Widget build(BuildContext context) {
+    final listChipWidget = ListView.builder(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.all(8.0),
+        itemCount: nameTechnologies.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            child: Transform(
+                transform: new Matrix4.identity()..scale(0.9),
+                child: Chip(
+                  label: Text(
+                    '${nameTechnologies[index]}',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  backgroundColor: Colors.blue,
+                )
+            ),
+          );
+        }
+    );
+
+    return Container(
+      height: 250.0,
+      child: Column(
+        children: <Widget>[
+//          listChipWidget,
+          Text(aboutSpeaker,style: TextStyle(color: Colors.blueGrey,fontSize: 18.0),),
+        ],
+      ),
+    );
+  }
+}
